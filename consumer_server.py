8fe8cc97fbcbb59eab8f37f46c0dba8afe7dc613
Andreas Santoso <andreas@theskimm.com>
2020-03-04 17:00:25 -0500

    Add initial consummer server logic

---
+++ consumer_server.py
@@ -0,0 +1,17 @@
+from pykafka import KafkaClient
+import logging
+
+logging.getLogger("pykafka.broker").setLevel('ERROR')
+
+client = KafkaClient(hosts="localhost:9092")
+topic = client.topics["police-calls"]
+
+consumer = topic.get_balanced_consumer(
+    auto_commit_enable = False,
+    zookeeper_connect = 'localhost:2181'
+)
+
+#Print out the messages
+for message in consumer:
+    if message is not None:
+        print( message.value.decode('utf-8') )
