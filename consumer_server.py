07b274524d850de049523b772f87dacd0267b77f
Andreas Santoso <andrebs.acad@gmail.com>
2020-03-13 19:32:18 -0400

    Add newline

--- consumer_server.py
+++ consumer_server.py
@@ -14,4 +14,4 @@ consumer = topic.get_balanced_consumer(
 #Print out the messages
 for message in consumer:
     if message is not None:
-        print(message.value.decode('utf-8'))
+        print(message.value.decode('utf-8')) 
