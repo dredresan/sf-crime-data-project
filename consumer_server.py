742d8231fb12a072d768079e3bcfdcc26a616e4c
Andreas Santoso <andrebs.acad@gmail.com>
2020-03-15 19:57:22 -0400

    Add consumer group

--- consumer_server.py
+++ consumer_server.py
@@ -8,10 +8,13 @@ topic = client.topics["police-calls"]
 
 consumer = topic.get_balanced_consumer(
     auto_commit_enable = False,
-    zookeeper_connect = 'localhost:2181'
+    zookeeper_connect = 'localhost:2181',
+    consumer_group = 'police_calls_consumer_group'
 )
 
-#Print out the messages
+# Print out the messages
+# This is similar to when consuming messages on the command line:
+# kafka-console-consumer --bootstrap-server localhost:9092 --topic police-calls --from-beginning
 for message in consumer:
     if message is not None:
-        print(message.value.decode('utf-8')) 
+        print(message.value.decode('utf-8'))
